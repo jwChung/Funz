14a3b7bf10befa3169e0e169eaa21fc503dc521b
Jin-Wook Chung <jwChung@hotmail.com>
2014-01-10 22:27:29 +0900

    Addressed the assembly discription.

--- src/Funz/Properties/AssemblyInfo.cs
+++ src/Funz/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Funz")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Funz is a variation of Funq to provide some features.")]
 [assembly: CLSCompliant(true)]
 [assembly: Guid("4b295c55-90f5-4f8c-bc35-3a9dadd4a36d")]
