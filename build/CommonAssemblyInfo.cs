ccfe39adf3e0303dcb17ca36ea3146a72c355495
Jin-Wook Chung <jwChung@hotmail.com>
2014-01-10 21:13:05 +0900

    Addressed assembly information file.

---
+++ build/CommonAssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Jin-Wook Chung")]
+[assembly: AssemblyCopyright("Copyright (c) 2014, Jin-Wook Chung")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyInformationalVersion("0.0.1")]
