2c9559a77f1d56f77c9d8702c3216354418d1084
Jin-Wook Chung <jwChung@hotmail.com>
2014-01-15 21:04:12 +0900

    Incremented minor version
    in order to indicate that this is the first release version.

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -11,4 +11,4 @@ using System.Runtime.InteropServices;
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyInformationalVersion("0.1.0-pre")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
