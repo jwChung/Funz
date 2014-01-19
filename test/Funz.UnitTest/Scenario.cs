93ea9f3ed1de1ece0137bd4dbd8ebe03c5cb72c9
Jin-Wook Chung <jwChung@hotmail.com>
2014-01-19 14:41:16 +0900

    Introduced some scenario tests
    for resolve, try-resolve and lazy-resolve.

--- test/Funz.UnitTest/Scenario.cs
+++ test/Funz.UnitTest/Scenario.cs
@@ -1,9 +1,63 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace Jwc.Funz
 {
     public class Scenario
     {
+        [VersionSpec(major: 0, minor: 1, patch: 0)]
+        public void ResolveRegisteredServiceReturnsCorrectInstance(
+            Container container)
+        {
+            // Fixture setup
+            container.Register<IFoo>(c => new Foo());
+
+            // Exercise system
+            var actual = container.Resolve<IFoo>();
+
+            // Verify outcome
+            Assert.IsType<Foo>(actual);
+        }
+
+        [VersionSpec(major: 0, minor: 1, patch: 0)]
+        public void ResolveUnregisteredServiceThrowsResoutionException(
+            Container container)
+        {
+            // Fixture setup
+            // Exercise system
+            // Verify outcome
+            Assert.Throws<ResolutionException>(() => container.Resolve<IFoo>());
+            Assert.Throws<ResolutionException>(() => container.Resolve<IBar>());
+        }
+
+        [VersionSpec(major: 0, minor: 1, patch: 0)]
+        public void TryResolveUnregisteredServiceReturnsDefaultValue(
+            Container container)
+        {
+            // Fixture setup
+            // Exercise system
+            var actual1 = container.TryResolve<IFoo>();
+            var actual2 = container.TryResolve<int>();
+
+            // Verify outcome
+            Assert.Null(actual1);
+            Assert.Equal(0, actual2);
+        }
+
+        [VersionSpec(major: 0, minor: 1, patch: 0)]
+        public void LazyResolveServiceReturnsFactory(
+            Container container)
+        {
+            // Fixture setup
+            container.Register<IFoo>(c => new Foo());
+
+            // Exercise system
+            Func<IFoo> actual = container.LazyResolve<IFoo>();
+
+            // Verify outcome
+            Assert.IsType<Foo>(actual.Invoke());
+        }
+
         [VersionSpec(major: 0, minor: 1, patch: 1)]
         public void ResolveRecursiveServiceThrowsResolutionException(
             Container container)
@@ -12,7 +66,8 @@ namespace Jwc.Funz
             container.Register<IFoo>(c => new Foo { Bar = c.Resolve<IBar>() });
             container.Register<IBar>(c => new Bar { Foo = c.Resolve<IFoo>() });
 
-            // Exercise system & Verify outcome
+            // Exercise system
+            // Verify outcome
             Assert.Throws<ResolutionException>(() => container.Resolve<IFoo>());
             Assert.Throws<ResolutionException>(() => container.Resolve<IBar>());
         }
