5d2e1ba7f23ca45567dff1024225c62838bf6973
Jin-Wook Chung <jwChung@hotmail.com>
2014-01-19 15:50:56 +0900

    Documented the IContainerVisitor interface.

--- src/Funz/IContainerVisitor.cs
+++ src/Funz/IContainerVisitor.cs
@@ -1,9 +1,21 @@
 ﻿namespace Jwc.Funz
 {
+    /// <summary>
+    /// Represents a contianer visitor.
+    /// </summary>
+    /// <typeparam name="TResult">The result type.</typeparam>
     public interface IContainerVisitor<out TResult>
     {
+        /// <summary>
+        /// Gets a value to indicating a result produced after visiting.
+        /// </summary>
         TResult Result { get; }
 
+        /// <summary>
+        /// Visits a container.
+        /// </summary>
+        /// <param name="container">The target container for visiting.</param>
+        /// <returns>The visitor to provide a result.</returns>
         IContainerVisitor<TResult> Visit(Container container);
     }
 }
