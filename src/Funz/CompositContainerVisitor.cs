e2a0b7dbb70c468ddd70cada4a1b5002b9f88a5b
Jin-Wook Chung <jwChung@hotmail.com>
2014-01-19 16:19:30 +0900

    Introduced the CompositContainerVisitor class.

---
+++ src/Funz/CompositContainerVisitor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Jwc.Funz
+{
+    public class CompositContainerVisitor<TResult> : IContainerVisitor<IEnumerable<TResult>>
+    {
+        public IEnumerable<TResult> Result
+        {
+            get;
+            private set;
+        }
+
+        public IContainerVisitor<IEnumerable<TResult>> Visit(Container container)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ test/Funz.UnitTest/CompositContainerVisitorTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Jwc.AutoFixture.Xunit;
+using Xunit;
+
+namespace Jwc.Funz
+{
+    public class CompositContainerVisitorTest
+    {
+        [Spec]
+        public void SutIsContainerVisitorOfEnumerable(
+            CompositContainerVisitor<string> sut)
+        {
+            // Fixture setup
+            // Exercise system
+            // Verify outcome
+            Assert.IsAssignableFrom<IContainerVisitor<IEnumerable<string>>>(sut);
+        }
+    }
+}
