41759486ba4133fd52621746d2d76760ca9897e9
Jin-Wook Chung <jwChung@hotmail.com>
2014-01-19 14:34:34 +0900

    Introduced the scenario test for v0.1.1.

---
+++ test/Funz.UnitTest/Scenario.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+
+namespace Jwc.Funz
+{
+    public class Scenario
+    {
+        [VersionSpec(major: 0, minor: 1, patch: 1)]
+        public void ResolveRecursiveServiceThrowsResolutionException(
+            Container container)
+        {
+            // Fixture setup
+            container.Register<IFoo>(c => new Foo { Bar = c.Resolve<IBar>() });
+            container.Register<IBar>(c => new Bar { Foo = c.Resolve<IFoo>() });
+
+            // Exercise system & Verify outcome
+            Assert.Throws<ResolutionException>(() => container.Resolve<IFoo>());
+            Assert.Throws<ResolutionException>(() => container.Resolve<IBar>());
+        }
+
+        private interface IFoo
+        {
+            IBar Bar { get; set; }
+        }
+
+        private interface IBar
+        {
+            IFoo Foo { get; set; }
+        }
+
+        private class Foo : IFoo
+        {
+            public IBar Bar
+            {
+                get;
+                set;
+            }
+        }
+
+        private class Bar : IBar
+        {
+            public IFoo Foo
+            {
+                get;
+                set;
+            }
+        }
+    }
+}
