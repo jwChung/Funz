5ddb47d274928bbb776f79abd456f9350d85e2b0
Jin-Wook Chung <jwChung@hotmail.com>
2014-01-19 14:28:16 +0900

    Introduced the VersionSpec attribute.

---
+++ test/Funz.UnitTest/VersionSpecAttribute.cs
@@ -0,0 +1,42 @@
+﻿using Jwc.AutoFixture.Xunit;
+
+namespace Jwc.Funz
+{
+    public class VersionSpecAttribute : SpecAttribute
+    {
+        private readonly int _major;
+        private readonly int _minor;
+        private readonly int _patch;
+
+        public VersionSpecAttribute(int major, int minor, int patch)
+        {
+            _major = major;
+            _minor = minor;
+            _patch = patch;
+        }
+
+        public int Major
+        {
+            get
+            {
+                return _major;
+            }
+        }
+
+        public int Minor
+        {
+            get
+            {
+                return _minor;
+            }
+        }
+
+        public int Patch
+        {
+            get
+            {
+                return _patch;
+            }
+        }
+    }
+}
