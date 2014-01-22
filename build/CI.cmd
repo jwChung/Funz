b0e5e2ff90b9f3de0885960009dbb8e2a8b38822
Jin-Wook Chung <jwChung@hotmail.com>
2014-01-23 08:51:25 +0900

    Addressed the CI script.

---
+++ build/CI.cmd
@@ -0,0 +1,10 @@
+@ECHO OFF
+set MsBuildPath="%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
+if exist "C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" (
+set MsBuildPath="C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe"
+)
+if exist "C:\Program Files\MSBuild\12.0\Bin\msbuild.exe" (
+set MsBuildPath="C:\Program Files\MSBuild\12.0\Bin\msbuild.exe"
+)
+
+%MsBuildPath% "build\Funz.proj" /v:normal /maxcpucount /nodeReuse:false /t:Publish /p:SetApiKey=%SetApiKey%;PrivateKey=%PrivateKey% %*
