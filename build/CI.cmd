d372bfdb94613e898bfbfa397d1bfabbd023e3b3
Jin-Wook Chung <jwChung@hotmail.com>
2014-01-23 09:46:52 +0900

    Addressed the logger
    to send MSBuild warnings and errors to AppVeyor and display them.

--- build/CI.cmd
+++ build/CI.cmd
@@ -7,4 +7,4 @@ if exist "C:\Program Files\MSBuild\12.0\Bin\msbuild.exe" (
 set MsBuildPath="C:\Program Files\MSBuild\12.0\Bin\msbuild.exe"
 )
 
-%MsBuildPath% "build\Funz.proj" /v:normal /maxcpucount /nodeReuse:false /t:Publish /p:SetApiKey=%SetApiKey%;PrivateKey=%PrivateKey% %*
+%MsBuildPath% "build\Funz.proj" /v:normal /maxcpucount /nodeReuse:false /t:Publish /p:SetApiKey=%SetApiKey%;PrivateKey=%PrivateKey% /logger:"%MSBuildLoggerPath%";"%MSBuildLogs%\buld-log-%Appveyor_ProjectBuildNumber%.xml" %*
