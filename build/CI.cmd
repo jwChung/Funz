fa78d9b3431f8d85550c84e0b5e76463e749ae8c
Jin-Wook Chung <jwChung@hotmail.com>
2014-02-25 00:41:12 +0900

    Addressed using system varibles
    for programs directory.

--- build/CI.cmd
+++ build/CI.cmd
@@ -1,10 +1,10 @@
 @ECHO OFF
 set MsBuildPath="%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
-if exist "C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" (
-set MsBuildPath="C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe"
+if exist "%ProgramFiles(x86)%\MSBuild\12.0\Bin\msbuild.exe" (
+set MsBuildPath="%ProgramFiles(x86)%\MSBuild\12.0\Bin\msbuild.exe"
 )
-if exist "C:\Program Files\MSBuild\12.0\Bin\msbuild.exe" (
-set MsBuildPath="C:\Program Files\MSBuild\12.0\Bin\msbuild.exe"
+if exist "%ProgramFiles%\MSBuild\12.0\Bin\msbuild.exe" (
+set MsBuildPath="%ProgramFiles%\MSBuild\12.0\Bin\msbuild.exe"
 )
 
 %MsBuildPath% "build\Funz.proj" /v:normal /maxcpucount /nodeReuse:false /t:Publish /p:SetApiKey=%SetApiKey%;PrivateKey=%PrivateKey% /logger:"%MSBuildLoggerPath%";"%MSBuildLogs%\buld-log-%Appveyor_ProjectBuildNumber%.xml" %*
