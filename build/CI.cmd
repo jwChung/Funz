e4fbb1df421e834aaa32d153e0e793826448218b
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-06 15:46:45 +0900

    Addressed the GitHubAccount parameter.

--- build/CI.cmd
+++ build/CI.cmd
@@ -1,3 +1,3 @@
 @ECHO OFF
 cd build
-Verify.cmd /t:Publish /p:SetApiKey=%SetApiKey%;PrivateKey=%PrivateKey% /logger:"%MSBuildLoggerPath%";"%MSBuildLogs%\buld-log-%Appveyor_ProjectBuildNumber%.xml"
+Verify.cmd /t:Publish /p:SetApiKey=%SetApiKey%;PrivateKey=%PrivateKey%;GitHubAccount=%GitHubAccount% /logger:"%MSBuildLoggerPath%";"%MSBuildLogs%\buld-log-%Appveyor_ProjectBuildNumber%.xml"
